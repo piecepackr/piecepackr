02366db11bbcb3fb1e469001f44562071c9fd38a
Trevor L Davis <trevor.l.davis@gmail.com>
2019-11-16 17:58:01 -0800

    feat: Add hexpack to 'game_systems()'

--- R/game_systems.R
+++ R/game_systems.R
@@ -1,9 +1,9 @@
 #' Standard game systems
 #'
 #' \code{game_systems} returns a list of \code{pp_cfg} objects
-#' of some game systems: the piecepack (plus a playing cards expansion and a subpack).
+#' of some game systems: the piecepack (plus a playing cards expansion, hexpack, and a "stackpack" subpack).
 #' @param style If \code{NULL} (the default) uses suit glyphs from the default \dQuote{sans} font.  
-#'        If \code{dejavu} uses suit glyphs from the "DejaVu Sans" font (must be installed on the system).
+#'        If \code{"dejavu"} it will use suit glyphs from the "DejaVu Sans" font (must be installed on the system).
 #' @examples
 #'        cfgs <- game_systems()
 #'        names(cfgs)
@@ -21,28 +21,36 @@
 #' @export
 game_systems <- function(style=NULL) {
     if (is.null(style)) {
-        piecepack <- list(suit_text="\u263c,\u25d8,\u0238,\u03ee,\u2202",  
-                          border_color="black", border_lex=4, depth.coin=0.25,
-                          mat_color.tile_back="white", mat_width.tile_back=0.05, suit_color.unsuited="black",
-                          rank_text=",a,2,3,4,5", use_suit_as_ace=TRUE)
-        playing_cards_expansion <- piecepack
-        playing_cards_expansion$suit_text <- "\u2665,\u2660,\u2663,\u2666,\u2202"
+        piecepack_suits <- list(suit_text="\u263c,\u25d8,\u0238,\u03ee,\u2202")
+        pce_suit_text <- "\u2665,\u2660,\u2663,\u2666,\u2202"
     } else if (style == "dejavu") {
-        piecepack <- list(suit_text="\u2742,\u25d0,\u265b,\u269c,\u0ed1", fontfamily="DejaVu Sans", 
-                          border_color="black", border_lex=4, dm_cex.coin=0.5, depth.coin=0.25, 
-                          mat_color.tile_back="white", mat_width.tile_back=0.05, suit_color.unsuited="black",
-                          rank_text=",a,2,3,4,5", use_suit_as_ace=TRUE)
-        playing_cards_expansion <- piecepack
-        playing_cards_expansion$suit_text <- "\u2665,\u2660,\u2663,\u2666,\u0ed1"
+        piecepack_suits <- list(suit_text="\u2742,\u25d0,\u265b,\u269c,\u0ed1", 
+                                suit_cex.s2=0.9,dm_cex.coin=0.5, fontfamily="DejaVu Sans")
+        pce_suit_text <- "\u2665,\u2660,\u2663,\u2666,\u0ed1"
     } else {
         stop(paste("Don't have a customized configuration for style", style))
     }
+    piecepack_base <- list(border_color="black", border_lex=4, depth.coin=0.25,
+                          mat_color.tile_back="white", mat_width.tile_back=0.05, suit_color.unsuited="black",
+                          rank_text=",a,2,3,4,5", use_suit_as_ace=TRUE)
+    piecepack <- c(piecepack_suits, piecepack_base)
+
+    playing_cards_expansion <- piecepack
+    playing_cards_expansion$suit_text <- pce_suit_text
     playing_cards_expansion$suit_color <- "#D55E00,#000000,#000000,#D55E00,#000000"
+
     subpack <- c(piecepack, list(width.tile=0.75, width.coin=0.3, width.die=0.3, 
                                  width.pawn=0.4 * 0.5, height.pawn=0.4 * 0.875,
                                  cex=0.4))
 
-    list(piecepack=pp_cfg(piecepack), playing_cards_expansion=pp_cfg(playing_cards_expansion),
+    hexpack <- c(piecepack, list(shape.tile="convex6", border_lex=3, 
+                                 shape_t.tile="60",  dm_t.tile_face=-90,
+                                 width.tile=4/sqrt(3), height.tile=4/sqrt(3),
+                                 shape.coin="convex3"))
+
+    list(hexpack=pp_cfg(hexpack), 
+         piecepack=pp_cfg(piecepack), 
+         playing_cards_expansion=pp_cfg(playing_cards_expansion),
          subpack=pp_cfg(subpack))
 }
 
